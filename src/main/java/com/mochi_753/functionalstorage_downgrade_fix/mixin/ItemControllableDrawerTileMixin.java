13269394762ffc88e7dc09e24dd96bd1589fb93f
mochi-753 <179483962+mochi-753@users.noreply.github.com>
2026-03-10 17:57:59 +0900

    ファイルサイズ軽量化

--- src/main/java/com/mochi_753/functionalstorage_downgrade_fix/mixin/ItemControllableDrawerTileMixin.java
+++ src/main/java/com/mochi_753/functionalstorage_downgrade_fix/mixin/ItemControllableDrawerTileMixin.java
@@ -1,16 +1,28 @@
 package com.mochi_753.functionalstorage_downgrade_fix.mixin;
 
+import com.buuz135.functionalstorage.FunctionalStorage;
 import com.buuz135.functionalstorage.block.tile.ItemControllableDrawerTile;
-import com.llamalad7.mixinextras.sugar.Local;
+import com.buuz135.functionalstorage.item.StorageUpgradeItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.RegistryObject;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = ItemControllableDrawerTile.class, remap = false)
 public abstract class ItemControllableDrawerTileMixin {
-    @ModifyConstant(method = "lambda$getStorageUpgradesConstructor$2", constant = @Constant(intValue = 64))
-    private int replaceIronUpgradeCheck(int constant, @Local(name = "i") int i) {
+    @Inject(method = "lambda$getStorageUpgradesConstructor$2", at = @At("HEAD"), cancellable = true)
+    private void onLambda$getStorageUpgradesConstructor$2(ItemStack stack, Integer integer, CallbackInfoReturnable<Boolean> cir) {
         ItemControllableDrawerTile<?> self = (ItemControllableDrawerTile<?>) (Object) this;
-        return self.getStorage().getStackInSlot(i).getMaxStackSize();
+
+        if (stack.getItem().equals(((RegistryObject<?>) FunctionalStorage.STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.IRON)).get())) {
+            for (int i = 0; i < self.getStorage().getSlots(); ++i) {
+                if (self.getStorage().getStackInSlot(i).getCount() > self.getStorage().getStackInSlot(i).getMaxStackSize()) {
+                    cir.setReturnValue(false);
+                    cir.cancel();
+                }
+            }
+        }
     }
 }
